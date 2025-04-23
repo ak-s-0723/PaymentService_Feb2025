8451d689477a2c6747b5e0e3b9d41ebf30ac6c1e
khannaanurag1 <khannaanurag1@gmail.com>
2025-04-23 08:25:09 +0530

    Initial Commit

---
+++ src/main/java/org/example/paymentservice_feb2025/PaymentServiceFeb2025Application.java
@@ -0,0 +1,13 @@
+package org.example.paymentservice_feb2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceFeb2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceFeb2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/paymentservice_feb2025/PaymentServiceFeb2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.paymentservice_feb2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceFeb2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
